1d0560f1e5062b91ad10d41f0a47623f3107329d
soerenmahr <soerenmahr.srm@gmail.com>
2025-12-03 14:44:34 -0800

    dwe

--- Codes/scan_averaging/domain/models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from pathlib import Path
-
-from Lab_apps._domain.models import ScanDataBase
-
-
-@dataclass(frozen=True)
-class AveragedScansData(ScanDataBase):
-    file_names: list[Path]
