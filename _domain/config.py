8f0f29c7a5b3d34b2ec65d9d700b7c8453eee1b4
soerenmahr <soerenmahr.srm@gmail.com>
2026-01-22 12:16:51 -0800

    delete useless config

--- _domain/config.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from base_core.quantities.enums import Prefix
-from base_core.quantities.models import Length
-
-
-
-DELAY_STAGE_CENTER_VALUE = Length(55, Prefix.MILLI)
