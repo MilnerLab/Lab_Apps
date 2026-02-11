7415311cd80346e5400def49a53fe69ecdfb3f7d
MilnerLab <milnerlab@protonmail.com>
2026-02-10 20:42:56 -0800

    rcparams for jet only

--- manuscript_plotting_scripts/ocs/jet_only.py
+++ manuscript_plotting_scripts/ocs/jet_only.py
@@ -1,7 +1,5 @@
 print('Code start!')
 from pathlib import Path
-import matplotlib as mpl
-
 from matplotlib import pyplot as plt
 
 from _data_io.dat_finder import DatFinder
@@ -78,98 +76,6 @@ configs_2.append(IonDataAnalysisConfig(
     transform_parameter= 0.73))
 
 #--------------------------------------------------------------------------------------------------
-#Update the matplotlib settings
-mpl.rcParams.update({
-#copied from physrev.mplstyle file
-    #"axes.prop_cycle": "(cycler('color', ['5d81b4', 'e09b24', '8eb031', 'eb6235', '8678b2', 'c46e1a', '5c9dc7', 'ffbf00', 'a5609c']) + cycler('ls', ['-', '--', '-.', (0, (1,0.85)), (0, (3, 1, 1, 1, 1, 1)), (0, (3, 1, 1, 1)), (0, (5, 1)), ':', (4, (10, 3))]))",  
-  
-    # --- Axes ---
-    "axes.titlesize": "medium",
-    "axes.labelsize": 9,
-    "axes.formatter.use_mathtext": True,
-    "axes.linewidth": 0.5,
-    #"axes.grid": True,
-    #"axes.grid.axis": "both",  # which axis the grid should apply to
-    #"axes.grid.which": "major",
-    #"axes.axisbelow" : True,
-    #"grid.alpha": 0.25,
-
-    # --- Grid lines ---
-    #"grid.linewidth": 0.5,
-    #"grid.linestyle": "dashed",
-    #"grid.color": "xkcd:light gray",
-
-    # --- Lines ---
-    "lines.linewidth": 0.5,
-    "lines.marker": "d",
-    "lines.markersize": 0.5,
-    "hatch.linewidth": 0.25,
-    "patch.antialiased": True,
-    
-    #---Errorbars---
-    "errorbar.capsize": 1,
-
-    # --- Ticks (X) ---
-    #"xtick.top": True,
-    "xtick.bottom": True,
-    "xtick.major.size": 3.0,
-    "xtick.minor.size": 1.5,
-    "xtick.major.width": 0.5,
-    "xtick.minor.width": 0.5,
-    "xtick.direction": "in",
-    "xtick.minor.visible": False,
-    #"xtick.major.top": True,
-    "xtick.major.bottom": True,
-    "xtick.minor.bottom": True,
-    "xtick.major.pad": 5.0,
-    "xtick.minor.pad": 5.0,
-    "xtick.labelsize": 9,
-
-    # --- Ticks (Y) ---
-    "ytick.left": True,
-    #"ytick.right": True,
-    "ytick.major.size": 3.0,
-    #"ytick.minor.size": 1.5,
-    "ytick.major.width": 0.5,
-    #"ytick.minor.width": 0.5,
-    "ytick.direction": "in",
-    #"ytick.minor.visible": True,
-    "ytick.major.left": True,
-    #"ytick.major.right": True,
-    #"ytick.minor.left": True,
-    "ytick.major.pad": 5.0,
-    #"ytick.minor.pad": 5.0,
-    "ytick.labelsize": 9,
-    
-    
-    # --- Legend ---
-    "legend.frameon": True,
-    "legend.fontsize": 8,
-    "legend.handlelength": 1.375,
-    "legend.labelspacing": 0.4,
-    "legend.columnspacing": 1,
-    "legend.facecolor": "white",
-    "legend.edgecolor": "white",
-    "legend.framealpha": 1,
-    "legend.title_fontsize": 8,
- 
-    # --- Figure size ---
-    "figure.figsize": (3.375, 3.6), #1- column fig
-    #"figure.figsize": (6.75, 6.75), #approx. 2- column fig
-    "figure.subplot.left": 0.125,
-    "figure.subplot.bottom": 0.175,
-    "figure.subplot.top": 0.95,
-    "figure.subplot.right": 0.95,
-    "figure.autolayout": True,
-
-    # --- Fonts (computer modern) ---
-    "text.usetex": False,       #<------------------------------------------------<-----<_<_<_ LATEX 
-    #"mathtext.fontset": "cm",
-    "font.family": "serif",
-    "font.serif": ["cmr10"]
-
-})
-
 
 
 
